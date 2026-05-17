1a24b27c73d77860ae7870fe5e43c8c9ae70ce52
Alex Szymanski <alex.szy.2@gmail.com>
2026-05-17 15:13:14 -0400

    Attempt 1 added

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,27 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// Step 5: Watering Control
+if (temperature > 80) {
+    console.log("Watering on")
+}
+    else {
+        console.log ("Watering off")
+    }
+//Step 6: Garden Lights Control
+if (timeOfDay == "evening" ) {
+    console.log("Lights on")
+}
+    else if (timeOfDay == "night" ) {
+     console.log("Lights on")
+}
+    else {
+        console.log ("Lights off")
+    }
+
+//Step 7: SOil Moisture Adjustment
+while (soilMoisture < 40) {
+    soilMoisture = soilMoisture + 5
+console.log (soilMoisture)
+}
